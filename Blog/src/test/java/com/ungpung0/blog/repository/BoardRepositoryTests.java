1f2ad858e8baef693f4f497806f7ac90d4567183
ungpung0 <ungpung0@gmail.com>
2022-11-22 20:25:29 +0900

    Modify: Add Paging Test
    # 페이징 관련 테스트 추가.

--- Blog/src/test/java/com/ungpung0/blog/repository/BoardRepositoryTests.java
+++ Blog/src/test/java/com/ungpung0/blog/repository/BoardRepositoryTests.java
@@ -1,9 +1,14 @@
 package com.ungpung0.blog.repository;
 
+import com.ungpung0.blog.dto.BoardDTO;
+import com.ungpung0.blog.dto.PageRequestDTO;
+import com.ungpung0.blog.dto.PageResultDTO;
 import com.ungpung0.blog.entity.BoardEntity;
+import com.ungpung0.blog.service.BoardService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +18,9 @@ import java.util.stream.IntStream;
 public class BoardRepositoryTests {
     @Autowired
     private BoardRepository boardRepository;
+
+    @Autowired
+    private BoardService boardService;
 
     @Test
     @Transactional
@@ -27,5 +35,16 @@ public class BoardRepositoryTests {
 
             boardRepository.save(board);
         });
+    }
+
+    @Test
+    public void pagingTest() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().pageCurr(1).pageSize(10).build();
+
+        PageResultDTO<BoardDTO, BoardEntity> pageResultDTO = boardService.getList(pageRequestDTO);
+
+        for(BoardDTO boardDTO : pageResultDTO.getPageDTOList()) {
+            System.out.println(boardDTO);
+        }
     }
 }
--- Blog/src/test/java/com/ungpung0/blog/service/BoardServiceTests.java
+++ Blog/src/test/java/com/ungpung0/blog/service/BoardServiceTests.java
@@ -1,11 +1,12 @@
 package com.ungpung0.blog.service;
 
 import com.ungpung0.blog.dto.BoardDTO;
+import com.ungpung0.blog.dto.PageRequestDTO;
+import com.ungpung0.blog.dto.PageResultDTO;
+import com.ungpung0.blog.entity.BoardEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 public class BoardServiceTests {
@@ -22,6 +23,25 @@ public class BoardServiceTests {
                 .build();
 
         boardService.register(boardDTO);
+    }
+
+    @Test
+    public void pagingListTest() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().pageCurr(1).pageSize(10).build();
+
+        PageResultDTO<BoardDTO, BoardEntity> pageResultDTO = boardService.getList(pageRequestDTO);
+
+        System.out.println("===== PageResultDTO =====");
+        System.out.println("PREV : " + pageResultDTO.isPrevious());
+        System.out.println("PREV : " + pageResultDTO.isNext());
+        System.out.println("TOTAL : " + pageResultDTO.getPageTotal());
+
+        System.out.println("===== Pages Data =====");
+        for(BoardDTO boardDTO : pageResultDTO.getPageDTOList())
+            System.out.println(boardDTO);
+
+        System.out.println("===== Pages Index Number =====");
+        pageResultDTO.getPageList().forEach(i -> System.out.println(i));
     }
 
 }
